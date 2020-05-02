b2a6a16d8a8ba66cea1c62ef6d459252b1d8e7aa
Otto Martikainen <martikainenotto@gmail.com>
2020-05-02 15:30:00 +0300

    Simplify and add tests

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ extern crate nalgebra as na;
 
 use std::fmt;
 use rand::{thread_rng, Rng};
-use rand::distributions::{WeightedIndex, Distribution};
-use na::{U1, U2, Dynamic, Matrix, Vector2, VecStorage};
+use na::{Vector4, Matrix4};
 
 macro_rules! one_sqrt_two {
     () => {
@@ -23,81 +22,33 @@ struct QBit {
 
 impl QBit {
     fn collapse(self: &Self, rng: &mut rand::rngs::ThreadRng) -> CBit {
-        // Intuition, measure QBit get beck CBit
-        // P(QBit (a, b) == CBit 0) = ||a||^2
-        let is_zero: bool = rng.gen_bool(self.a.norm().powi(2));
+        // Intuition, measure QBit get back CBit
+        let probability = self.a.norm().powi(2);
+        //println!("P(is Zero) == ||{}|| ^ 2 == {}", self.a, probability);
+        //println!("P(is One) == ||{}|| ^ 2 == {}", self.b, (1.0-probability));
+        let is_zero: bool = rng.gen_bool(probability);
         if is_zero {
             return CBit::Zero
         }
         return CBit::One
+    }
+
+    fn bit_flip(self: &Self) -> QBit {
+        return QBit { a: self.b, b: self.a }
+    }
+    
+    fn hadamard(self: &Self) -> QBit {
+        let c = cplx(one_sqrt_two!());
+        return QBit {
+            a: c * self.a + c * self.b,
+            b: c * self.a - c * self.b
+        }
     }
 }
 
 impl fmt::Display for QBit {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "({}, {})", self.a, self.b)
-    }
-}
-
-type QBitVectorComponents = Matrix<num::complex::Complex64, U2, Dynamic, VecStorage<num::complex::Complex64, U2, Dynamic>>;
-
-#[derive(Debug)]
-struct QBitVector {
-    components: QBitVectorComponents
-}
-
-impl QBitVector {
-    fn collapse(self: &Self, rng: &mut rand::rngs::ThreadRng) -> CBitVector {
-        // Intuition, only one can be true
-        let component_probabilities: Vec<f64> = self.components.iter()
-            // P(QBit (a, b) == CBit 0) = ||a||^2
-            .map(|c| c.norm().powi(2))
-            .collect();
-        let distribution: WeightedIndex<f64> = WeightedIndex::new(&component_probabilities).unwrap();
-        let selected_component_index = distribution.sample(rng);
-        
-        let mut selected_bit_index = selected_component_index / 2; // Even
-        if selected_component_index % 2 == 1 { // Odd
-            selected_bit_index = (selected_component_index - 1) / 2;
-        }
-
-        let size = self.components.nrows();
-        let mut result: Vec<CBit> = Vec::with_capacity(size);
-        result.resize(self.components.nrows(), CBit::Zero);
-        result[selected_bit_index] = CBit::One;
-        return CBitVector {
-            bits: CBitVectorBits::from_iterator(size, result.into_iter()) }
-    }
-
-    fn bit_flip(self: &Self) -> QBitVector {
-        let matrix = QBitVectorComponents::from_columns(&[
-            qbit_1(),
-            qbit_0()
-        ]);
-        return QBitVector {
-            components: matrix * self.components.clone()
-        }
-    }
-    
-    fn hadamard(self: &Self) -> QBitVector {
-        let matrix = QBitVectorComponents::from_columns(&[
-            Vector2::new(cplx(one_sqrt_two!()), cplx(one_sqrt_two!())),
-            Vector2::new(cplx(one_sqrt_two!()), cplx(-one_sqrt_two!()))
-        ]);
-        return QBitVector {
-            components: matrix * self.components.clone()
-        }
-    }
-}
-
-impl fmt::Display for QBitVector {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.components
-            .column_iter()
-            .for_each(|column| {
-                write!(f, "\n({}, {})", column[0], column[1]);
-            });
-        Ok(())
     }
 }
 
@@ -120,57 +71,101 @@ impl PartialEq for CBit {
 impl fmt::Display for CBit {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            CBit::Zero => write!(f, "0"),
-            CBit::One => write!(f, "1")
+            CBit::Zero => write!(f, "|0>"),
+            CBit::One => write!(f, "|1>")
         }
     }
 }
 
-type CBitVectorBits = Matrix::<CBit, U1, Dynamic, VecStorage<CBit, U1, Dynamic>>;
-
-#[derive(Debug)]
-struct CBitVector {
-    bits: CBitVectorBits
+fn tensor_product(a: &QBit, b: &QBit) -> Vector4<num::complex::Complex64> {
+    // Kronecker product (a.a, a.b) * (b.a, b.b)
+    Vector4::new(
+        a.a * b.a,
+        a.a * b.b,
+        a.b * b.a,
+        a.b * b.b
+    ) 
 }
 
-impl fmt::Display for CBitVector {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.bits
-            .column_iter()
-            .for_each(|column| {
-                write!(f, "\n{}", column[0]);
-            });
-        Ok(())
+fn cnot(inputs: Vector4<num::complex::Complex64>) -> Vector4<num::complex::Complex64> {
+    let matrix = Matrix4::new(
+        cplx(1.0), cplx(0.0), cplx(0.0), cplx(0.0),
+        cplx(0.0), cplx(1.0), cplx(0.0), cplx(0.0),
+        cplx(0.0), cplx(0.0), cplx(0.0), cplx(1.0),
+        cplx(0.0), cplx(0.0), cplx(1.0), cplx(0.0),
+    );
+    matrix * inputs
+}
+
+
+fn tensor_deproduct(inputs: Vector4<num::complex::Complex64>) -> (QBit, QBit) {
+    // FIXME: use inverse of Kronecker product
+    // to get back bit pair representation
+    // https://math.stackexchange.com/a/60441
+    (
+        QBit {
+            a: inputs[0],
+            b: inputs[1]
+        },
+        QBit {
+            a: inputs[2],
+            b: inputs[3]
+        }
+    )
+}
+
+
+
+fn main() {
+    test_op(op_constant_0, CBit::One, CBit::One, "op_constant_0");
+    test_op(op_constant_1, CBit::One, CBit::One, "op_constant_1");
+    test_op(op_identity, CBit::Zero, CBit::One, "op_identity");
+    test_op(op_negation, CBit::Zero, CBit::One, "op_negation");
+}
+
+fn test_op(op: fn(QBit, QBit) -> (QBit, QBit), expect_output: CBit, expect_input: CBit, op_name: &str) -> () {
+    let mut rng = thread_rng();
+
+    let output = qbit_0()
+        .bit_flip()
+        .hadamard();
+    let input = qbit_0()
+        .bit_flip()
+        .hadamard();
+
+    let (output_after, input_after) = op(output, input);
+
+    let output_result = output_after.hadamard().collapse(&mut rng);
+    let input_result = input_after.hadamard().collapse(&mut rng);
+    if output_result != expect_output {
+        panic!("{}: output {} did not match expectation {}", op_name, output_result, expect_output);
+    }
+    if input_result != expect_input {
+        panic!("{}: input {} did not match expectation {}", op_name, input_result, expect_input);
     }
 }
 
-/*
-fn cnot(control: &QBit, value: &QBit) -> QBit {
-    if (control)
+
+fn op_constant_0(output: QBit, input: QBit) -> (QBit, QBit) {
+    (output, input)
 }
-*/
 
-fn main() {
-    let mut rng = thread_rng();
+fn op_constant_1(output: QBit, input: QBit) -> (QBit, QBit) {
+    (output.bit_flip(), input)
+}
 
-    let coin = QBit {
-        a: cplx(one_sqrt_two!()),
-        b: cplx(one_sqrt_two!())
-    };
-    println!("QBit {}, collapses to CBit {}", coin, coin.collapse(&mut rng));
+fn op_identity(output: QBit, input: QBit) -> (QBit, QBit) {
+    let v = tensor_product(&input, &output);
+    let v_result = cnot(v);
+    let (input_result, output_result) = tensor_deproduct(v_result);
+    (output_result, input_result)
+}
 
-    let pair = qbit_vector(&[qbit_super(), qbit_super()]);
-
-    println!("QBitVector {}, collapses to CBitVector {}", pair, pair.collapse(&mut rng));
-
-    let classical = qbit_vector(&[
-        qbit_0(),
-        qbit_1(),
-        qbit_super()
-    ]);
-
-    println!("QBitVector {}, after hadamard {}", classical, classical.hadamard());
-    println!("QBitVector {}, after bitflip {}", classical, classical.bit_flip());
+fn op_negation(output: QBit, input: QBit) -> (QBit, QBit) {
+    let v = tensor_product(&input, &output);
+    let v_result = cnot(v);
+    let (input_result, output_result) = tensor_deproduct(v_result);
+    (output_result.bit_flip(), input_result)
 }
 
 
@@ -182,20 +177,14 @@ fn cplx(real: f64) -> num::complex::Complex64 {
     num::complex::Complex::new(real, 0.0)
 }
 
-fn qbit_vector(bits: &[Vector2<num::complex::Complex64>]) -> QBitVector {
-    QBitVector {
-        components: QBitVectorComponents::from_columns(bits)
-    }
+fn qbit_0() -> QBit {
+    QBit {a: cplx(1.0), b: cplx(0.0) }
 }
 
-fn qbit_0() -> Vector2<num::complex::Complex64> {
-    Vector2::new(cplx(1.0), cplx(0.0))
+fn qbit_1() -> QBit {
+    QBit {a: cplx(0.0), b: cplx(1.0) }
 }
 
-fn qbit_1() -> Vector2<num::complex::Complex64> {
-    Vector2::new(cplx(0.0), cplx(1.0))
-}
-
-fn qbit_super() -> Vector2<num::complex::Complex64> {
-    Vector2::new(cplx(one_sqrt_two!()), cplx(one_sqrt_two!()))
+fn qbit_super() -> QBit{
+    QBit {a: cplx(one_sqrt_two!()), b: cplx(one_sqrt_two!())}
 }
